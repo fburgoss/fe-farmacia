0e661ba6d706cbcadf3cdcb79f10d64d58597cfc
francisco burgos <f.burgos1589@gmail.com>
2025-07-22 13:05:09 -0400

    add card home

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,20 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Medicamento, MedicamentoService } from '../medicamentos/medicamento.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [CommonModule, MatTabsModule, MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home implements OnInit {
 
   selectedIndex = 0;
+  productos: Medicamento[] = [];
+
+  constructor(private medicamentoService: MedicamentoService) { }
 
   ngOnInit(): void {
     setInterval(() => {
-      this.selectedIndex = (this.selectedIndex + 1) % 3; // 3 porque tienes 3 tabs
-    }, 3000); // cada 3 segundos
+      this.selectedIndex = (this.selectedIndex + 1) % 3;
+    }, 3000);
+
+    this.medicamentoService.getMedicamentos().subscribe(data => {
+      this.productos = data;
+    });
   }
 }
